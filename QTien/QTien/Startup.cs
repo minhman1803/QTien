a1c2d7f711940409b2cb5fb737bee9111502ed1a
US <minhman1803>
2018-11-21 10:52:50 +0700

    hihihi

---
+++ QTien/QTien/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QTien.Startup))]
+namespace QTien
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
